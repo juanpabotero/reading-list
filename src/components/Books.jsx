c18fdbf7fb3d3df3463b220fd207a38bbbfdfb79
juanpabotero <juanpabotero1@gmail.com>
2023-07-27 18:52:16 -0500

    fix: change buttons colors

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -24,8 +24,8 @@ export function Books({ books }) {
               className={`absolute top-0 right-0 rounded-full p-1 border bg-gray-900 
               hover:scale-110 transition-all duration-200 ${
                 isBookInReadingList
-                  ? 'text-yellow-500 border-yellow-600'
-                  : 'text-blue-500 border-blue-600'
+                  ? 'text-lime-400 border-lime-600'
+                  : 'text-yellow-400 border-yellow-600'
               } add-book-button`}
               onClick={() => {
                 isBookInReadingList
